a76c337b5e2b720e19d23dada549295483bc1c6a
Marsani <marsanix@gmail.com>
2024-10-11 15:51:06 +0700

    perubahan file main.kt

--- src/Main.kt
+++ src/Main.kt
@@ -3,8 +3,8 @@ fun main() {
     println("Tugas Pemrograman Dasar Kotlin!")
 
     // Silahkan isi data diri Anda pada variable berikut:
-    val nama = "Sani"
-    val sekolah = "UNSIA"
+    val nama = ""
+    val sekolah = ""
 
 
 }
